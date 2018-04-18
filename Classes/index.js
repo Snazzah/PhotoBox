2f3a2c66e2c552837ce8210bc8062f61d2737cbe
Snazzah <suggesttosnazzy@gmail.com>
2018-04-18 01:31:27 -0500

    more cleanup

--- Classes/index.js
+++ Classes/index.js
@@ -2,7 +2,6 @@ module.exports = {
   Command: require('./Command'),
   TextCommand: require('./TextCommand'),
   PhotoCommand: require('./PhotoCommand'),
-  IsNowIllegal: require('./IsNowIllegal'),
   Pix2Pix: require('./Pix2Pix'),
   ImageCode: require('./ImageCode')
 }
