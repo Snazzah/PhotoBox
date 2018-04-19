4f4d4b59ccea896ed608dad5e709be8d2a556011
Snazzah <suggesttosnazzy@gmail.com>
2018-04-19 05:55:38 -0500

    📷waifu

---
+++ Commands/templates/waifu.js
@@ -0,0 +1,13 @@
+const { PhotoCommand } = require('photobox')
+const { Util } = require('photobox-core')
+
+module.exports = class Waifu extends PhotoCommand {
+  get name() { return 'waifu' }
+  get aliases() { return ['trashwaifu','trash'] }
+
+  get helpMeta() { return {
+    category: 'Image Manipulation',
+    description: 'Use this if you think a waifu is trash.',
+    usage: '[url]'
+  } }
+}
