8ddc137be9e47594ac1ba88a7c6b13d66b320093
Snazzah <suggesttosnazzy@gmail.com>
2018-04-23 03:00:10 -0500

    Resize image in 📷magik since it takes a long time to generate

--- ImageCodes/magik.js
+++ ImageCodes/magik.js
@@ -1,11 +1,12 @@
 const { ImageCode } = require('photobox')
-const path = require('path')
+const Jimp = require('jimp')
 const im = require('gm').subClass({ imageMagick: true })
 
 module.exports = class magik extends ImageCode {
   async process(msg) {
-    let avatar = im(await this.toBuffer(msg.avatar))
-    avatar.setFormat('png')
+    let ravatar = await Jimp.read(msg.avatar)
+    ravatar.resize(Jimp.AUTO, 512)
+    let avatar = im(await this.toBuffer(ravatar))
     avatar.out('-liquid-rescale').out('180%')
     avatar.out('-liquid-rescale').out('60%')
     this.sendIM(msg, avatar)
