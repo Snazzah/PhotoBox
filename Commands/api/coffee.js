9b136551cef4472070222de55f7bfe3fba55c7cd
Snazzah <suggesttosnazzy@gmail.com>
2018-05-24 17:57:26 -0500

    📷coffee

---
+++ Commands/api/coffee.js
@@ -0,0 +1,12 @@
+const { APICommand } = require('photobox')
+
+module.exports = class Coffee extends APICommand {
+  get name() { return 'coffee' }
+  get aliases() { return ['☕'] }
+  get url() { return 'https://coffee.alexflipnote.xyz/random.json' }
+  getImage(res) { return res.body.file }
+  get helpMeta() { return {
+    category: 'API',
+    description: 'Get coffee.'
+  } }
+}
