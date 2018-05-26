d16380aa3e4f4f4bc02ddf4d55a51aeda684ee4d
Snazzah <suggesttosnazzy@gmail.com>
2018-05-25 22:30:15 -0500

    use different url for "cat"

--- Commands/api/singleserve/cat.js
+++ Commands/api/singleserve/cat.js
@@ -3,8 +3,8 @@ const { APICommand } = require('photobox')
 module.exports = class Cat extends APICommand {
   get name() { return 'cat' }
   get aliases() { return ['🐱', '😿', '😻', '😹', '😽', '😾', '🙀', '😸', '😺', '😼'] }
-  get url() { return 'https://aws.random.cat/meow' }
-  getImage(res) { return res.body.file }
+  get url() { return 'https://nekos.life/api/v2/img/meow' }
+  getImage(res) { return res.body.url }
   get helpMeta() { return {
     category: 'API',
     description: 'Get a random cat.'
