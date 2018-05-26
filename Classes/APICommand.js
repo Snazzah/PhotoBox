9f0c3f33f9ce49e7c20c35cfcc04edc1664ce3ef
Snazzah <suggesttosnazzy@gmail.com>
2018-05-25 22:29:56 -0500

    eXtensive error handling

--- Classes/APICommand.js
+++ Classes/APICommand.js
@@ -5,12 +5,43 @@ module.exports = class APICommand extends Command {
   get cooldown() { return 1 }
 
   async exec(message, args) {
-    let res = await sf.get(this.url)
+    let done = null
+    this.doTimer(message, d => done = d)
+    try{
+      let res = await sf.get(this.url)
 
-    message.channel.send({ embed: {
-      color: 0x9acccd,
-      image: { url: this.getImage(res) }
-    }})
+      await message.channel.send({ embed: {
+        color: 0x9acccd,
+        image: { url: this.getImage(res) }
+      }})
+
+      done()
+      message.channel.stopTyping()
+    }catch(e){
+      if(done(true)) return
+      await message.reply(`The service gave us a ${e.statusCode}! Try again later!`)
+      done()
+      message.channel.stopTyping()
+    }
+  }
+
+  doTimer(message, func) {
+    let done = false
+    let quit = false
+    func(d => {
+      if(d) return quit
+      done = true
+    })
+    setTimeout(() => {
+      if(!done) message.channel.startTyping()
+    }, 1000)
+    setTimeout(() => {
+      if(!done) {
+        quit = true
+        message.reply('The request was dropped due to the call taking too long!')
+        message.channel.stopTyping()
+      }
+    }, 10000)
   }
 
   get permissions() { return ['embed'] }
