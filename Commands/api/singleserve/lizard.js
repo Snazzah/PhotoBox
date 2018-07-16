d32b8a29cf55076c2f29b2edbbb61e35cde1e855
Snazzah <suggesttosnazzy@gmail.com>
2018-07-16 08:25:13 -0500

    📷lizard

---
+++ Commands/api/singleserve/lizard.js
@@ -0,0 +1,12 @@
+const { APICommand } = require('photobox')
+
+module.exports = class Lizard extends APICommand {
+  get name() { return 'lizard' }
+  get aliases() { return ['🦎'] }
+  get url() { return 'https://nekos.life/api/v2/img/lizard' }
+  getImage(res) { return res.body.url }
+  get helpMeta() { return {
+    category: 'API',
+    description: 'Get a random lizard.'
+  } }
+}
