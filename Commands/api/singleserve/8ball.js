e78d3c73024b366064c1b32a69bb75c607a4993e
Snazzah <suggesttosnazzy@gmail.com>
2018-07-16 08:48:11 -0500

    📷8ball

---
+++ Commands/api/singleserve/8ball.js
@@ -0,0 +1,12 @@
+const { APICommand } = require('photobox')
+
+module.exports = class EightBall extends APICommand {
+  get name() { return '8ball' }
+  get aliases() { return ['🎱'] }
+  get url() { return 'https://nekos.life/api/v2/img/8ball' }
+  getImage(res) { return res.body.url }
+  get helpMeta() { return {
+    category: 'API',
+    description: 'Ask the eight ball.'
+  } }
+}
