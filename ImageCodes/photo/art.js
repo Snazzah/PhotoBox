1291ba2f8e149c0408c6f9be2bbfc86a2bfc0701
Snazzah <suggesttosnazzy@gmail.com>
2020-01-26 18:50:08 -0600

    Add a color background for 📷art

--- ImageCodes/photo/art.js
+++ ImageCodes/photo/art.js
@@ -7,7 +7,7 @@ module.exports = class art extends ImageCode {
     avatar.resize(370, 370);
 
     const foreground = await Jimp.read(this.resource('art.png'));
-    const img = new Jimp(1364, 1534);
+    const img = new Jimp(1364, 1534, 0xfbe7fcff);
     img.composite(avatar, 903, 92).composite(avatar, 903, 860).composite(foreground, 0, 0);
 
     this.sendJimp(msg, img);
