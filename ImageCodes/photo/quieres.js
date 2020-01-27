038399b7e59b4a6319274afde57eb992bdd3c6fa
Snazzah <suggesttosnazzy@gmail.com>
2020-01-26 20:41:09 -0600

    Resize quieres hand

--- ImageCodes/photo/quieres.js
+++ ImageCodes/photo/quieres.js
@@ -6,6 +6,8 @@ module.exports = class quieres extends ImageCode {
   async process(msg) {
     const img = await Jimp.read(msg.url);
     const hand = await Jimp.read(path.join(__dirname, '..', '..', 'assets', 'quieres.png'));
+    const handSize = img.bitmap.height > img.bitmap.width ? img.bitmap.width : img.bitmap.height;
+    hand.resize(handSize, Jimp.AUTO);
     img.composite(hand, img.bitmap.width - hand.bitmap.width, img.bitmap.height - hand.bitmap.height);
 
     this.sendJimp(msg, img);
