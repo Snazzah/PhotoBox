02e148de09ba2a1898abff9f0373834c951bc3d8
Snazzah <suggesttosnazzy@gmail.com>
2020-02-28 20:58:12 -0600

    Add benchmark tool

---
+++ benchmark/index.js
@@ -0,0 +1,62 @@
+const ora = require('ora');
+const fs = require('fs');
+const path = require('path');
+const config = require('config');
+const Util = require('./util');
+
+const { ImageProcess } = require('photobox-core');
+const IP = new ImageProcess();
+process.env.LOGGER_DEBUG = 'false';
+
+const BenchmarkConstants = {
+  PICTURE1: fs.readFileSync(path.join(__dirname, './resources/picture1.jpg')),
+  PICTURE2: fs.readFileSync(path.join(__dirname, './resources/picture2.jpg')),
+  NORMAL_TEXT: 'Making a 140-character sentence is not always that easy when you are constantly thinking of words to use that may or may not be cool to use.',
+  USERNAME: 'PhotoBox',
+};
+
+const imageCodesPaths = Util.flatten(Util.iterateFolder(path.resolve(config.get('image_codes'))));
+const imageCodes = imageCodesPaths.map(codePath => {
+  const imageCode = require(codePath);
+  return {
+    path: codePath,
+    code: path.parse(codePath).name,
+    message: imageCode.benchmark ? imageCode.benchmark(BenchmarkConstants) : null,
+  };
+});
+
+const counters = {
+  passed: 0,
+  skipped: 0,
+  failed: 0,
+};
+
+const startTime = Date.now();
+
+(async () => {
+  for (let i = 0, len = imageCodes.length; i < len; i++) {
+    const imageCode = imageCodes[i];
+
+    if(!imageCode.message) {
+      counters.skipped++;
+      continue;
+    }
+
+    const spinner = ora(imageCode.code).start();
+    const codeStartTime = Date.now();
+
+    try {
+      await IP.send({
+        code: imageCode.code,
+        ...imageCode.message,
+      });
+      counters.passed++;
+      spinner.succeed(`${imageCode.code}: ${Date.now() - codeStartTime} ms`);
+    } catch (e) {
+      counters.failed++;
+      spinner.fail(`${imageCode.code}: ${Date.now() - codeStartTime} ms`);
+    }
+  }
+
+  console.log(`\n- ${counters.failed ? '(!) ' : ''} took ${Date.now() - startTime} ms, ${counters.passed} passed, ${counters.failed} failed, ${counters.skipped} skipped`);
+})();
