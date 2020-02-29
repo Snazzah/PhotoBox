1a3bae84d2d79f9b83882fee831bb13e13f013ba
Snazzah <suggesttosnazzy@gmail.com>
2020-02-29 03:08:27 -0600

    small fix on svg conversions

--- ImageCodes/tools/svgToPNG.js
+++ ImageCodes/tools/svgToPNG.js
@@ -14,6 +14,6 @@ module.exports = class svgToPNG extends ImageCode {
     const metadata = await sharp(Buffer.from(msg.svg)).metadata();
     const maxSize = metadata.width > metadata.height ? metadata.width : metadata.height;
     const densityPerPixel = metadata.density / maxSize;
-    this.sendBuffer(msg, await sharp(Buffer.from(msg.svg), { density: densityPerPixel * size }).png());
+    this.send(msg, await sharp(Buffer.from(msg.svg), { density: densityPerPixel * size }).png());
   }
 };
